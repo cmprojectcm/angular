cef646471ed924fac653ff30d6306a74794cc322
cmprojects <christosmichaelcs@gmail.com>
2020-08-05 08:01:27 +0300

    adding map component initial

---
+++ src/app/map-component/map-component.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-component',
+  templateUrl: './map-component.component.html',
+  styleUrls: ['./map-component.component.css'],
+})
+export class MapComponent implements OnInit {
+  mapComponent = 'mappppp';
+  constructor() {}
+
+  ngOnInit(): void {}
+}
